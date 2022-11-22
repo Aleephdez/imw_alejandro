9da8e1fd76f6edc706a23ea62a75c66183bc4877
alejandro <alejandro.pazhdez@gmail.com>
2022-11-22 12:18:30 +0000

    [U]

--- trim-1/ut2/a3/program3.py
+++ trim-1/ut2/a3/program3.py
@@ -4,7 +4,7 @@ import sys
 num1 = int(sys.argv[1])
 num2 = int(sys.argv[2])
 
-#Por definición, los números primos deben ser positivos, así que comprobamos que el usuario haya introducido un número válido
+#El MCD está definido para los números naturales, así que no admitimos negativos.
 if num1 < 0 or num2 < 0:
     print("[*] Introduce un número positivo")
     sys.exit(0)
--- trim-1/ut2/a3/program4.py
+++ trim-1/ut2/a3/program4.py
@@ -3,18 +3,19 @@ import sys
 #Leemos el número introducido e inicializamos la variable total
 num = int(sys.argv[1])
 
-#Por definición, los números primos deben ser positivos, así que comprobamos que el usuario haya introducido un número válido
+#El factorial está definido para número naturales, así que no admitimos negativos
 if num < 0:
     print("[*] Introduce un número positivo")
     sys.exit(0)
+#Por definición, el factorial de 0 es 1
 elif num == 0:
     print("0! = 1")
     sys.exit(0)
 
-
+#Hacemos dos bucles, el primero se encarga de recorrer los números de los que haremos el factorial
 for i in range(1, num+1):
     factorial = 1
-    for j in range(i,0, -1):    
+    for j in range(i,0, -1):    #el segundo calcula el factorial de cada número recorrido por el primer bucle
         factorial *= j
     
     print("{}! = {}".format(i,factorial))
