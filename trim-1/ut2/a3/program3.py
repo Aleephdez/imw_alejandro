567c9bd7aebd9269b3750cff935c07d0b8c88f19
alejandro <alejandro.pazhdez@gmail.com>
2022-11-22 11:22:04 +0000

    [U]

--- trim-1/ut2/a3/program3.py
+++ trim-1/ut2/a3/program3.py
@@ -9,3 +9,24 @@ if num1 < 0 or num2 < 0:
     print("[*] Introduce un número positivo")
     sys.exit(0)
 
+if num1 == 0:
+
+    print('El MCD de', num1, 'y', num2, 'es', num2)
+
+elif num2 == 0:
+
+    print('El MCD de', num1, 'y', num2, 'es', num1)
+    
+else: #En caso de no tener ningún número igual a 0, calculamos el MCD
+   
+   #Calculamos el máximo y mínimo entre ambos números, ya que usaremos el número
+   #menor para operar sobre él
+    a = max(num1, num2) 
+    b = min(num1, num2)
+    
+    while b != 0:
+        resto = b
+        b = a%b
+        a = resto
+    
+    print('[+]El MCD de', num1, 'y', num2, 'es', a)
