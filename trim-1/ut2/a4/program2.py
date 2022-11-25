4a6d3c25a4add47930ab26f077149b2693e298ce
alejandro <alejandro.pazhdez@gmail.com>
2022-11-25 13:35:20 +0000

    [U]

--- trim-1/ut2/a4/program2.py
+++ trim-1/ut2/a4/program2.py
@@ -1,16 +1,18 @@
 import random
 
+#Generamos la secuencia ADN de forma aleatoria
 NUCLEOBASES = "ATGC"
 DNA_SIZE = 100
 
 sequence = ''.join([random.choice(NUCLEOBASES)for i in range(DNA_SIZE)])
 
+#Iniciamos las variables a 0
 adenine = 0
 guanine = 0
 cytosine = 0
 thymine = 0
 
-
+#Recorremos la secuencia de ADN analizando cada letra, si hay coincidencia acumulamos en la variable correspondiente
 for i in sequence:
     if i == 'A':
         adenine += 1
@@ -21,6 +23,7 @@ for i in sequence:
     elif i == 'T':
         thymine += 1
 
+#Imprimimos resultados
 print("[+] Adenine: {}".format(adenine))
 print("[+] Guanine: {}".format(guanine))
 print("[+] Cytosine: {}".format(cytosine))
--- trim-1/ut2/a4/program3.py
+++ trim-1/ut2/a4/program3.py
@@ -1,7 +1,9 @@
 import sys
 
+#Iniciamos la variable contador
 counter = 0
 
+#Leemos por pantalla
 k = int(sys.argv[1])
 
 string = sys.argv[2]
@@ -11,13 +13,17 @@ if len(sys.argv) != 3:
 if k <= 0:
     print("Introduce un número positivo distinto de 0")
 
+#Separamos las palabras de la cadena para analizarlas una a una
 word_list = string.split()
 
+#Calculamos la longitud de la lista
 list_size = len(word_list)
 
+#Recorremos la lista y comprobamos si la longitud de cada palabra es igual a la cantidad introducida
 for i in range(list_size):
     
     if len(word_list[i]) == k:
         counter += 1
 
+#Imprimimos por pantalla
 print("Hay {} palabras de tamaño {}".format(counter, k))
